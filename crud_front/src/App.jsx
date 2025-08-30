4416a78d472892a1420d1c21c1c5119545580eca
Maturu Sravan Kumar <148370583+sravan7979@users.noreply.github.com>
2025-08-30 12:22:49 +0530

    Update BASE_URL to production server address

--- crud_front/src/App.jsx
+++ crud_front/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://localhost:9090/springapp1';
+const BASE_URL = 'http://18.211.17.102:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
