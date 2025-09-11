9e434b4dd297411309351ca791f248e2e73074a7
Maturu Sravan Kumar <148370583+sravan7979@users.noreply.github.com>
2025-09-11 23:48:46 +0530

    url updation

--- crud_front/src/App.jsx
+++ crud_front/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://18.211.17.102:9090/springapp1';
+const BASE_URL = 'http://13.53.59.109:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
